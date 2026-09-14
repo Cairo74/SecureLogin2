2bd1c06c0d136d04278966640aa12acdc0e70587
Cairo Rodrigues Rezende <1647288@sga.pucminas.br>
2026-09-14 19:56:01 -0300

    first commit

---
+++ src/main/java/com/example/SecureLogin2/SecureLogin2Application.java
@@ -0,0 +1,13 @@
+package com.example.SecureLogin2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureLogin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureLogin2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SecureLogin2/SecureLogin2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecureLogin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureLogin2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
